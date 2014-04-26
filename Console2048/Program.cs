bf560db05bb51bddc3d1a214a5c5f28a8f769b22
IanWold <ilwold@hotmail.com>
2014-04-26 10:33:55 -0500

    First Commit
    Everything's here

---
+++ Console2048/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C = System.Console;
+
+namespace Console2048
+{
+    class Program
+    {
+        static int[,] Grid = new int[4, 4];
+
+        static void Main(string[] args)
+        {
+            ConsoleKey Input = ConsoleKey.A;
+            AddNumber();
+
+            while (true)
+            {
+                try
+                {
+                    AddNumber();
+                    C.Clear();
+                    WriteGrid();
+                    C.WriteLine();
+                    C.Write(">: ");
+
+                    Input = C.ReadKey().Key;
+
+                    switch (Input)
+                    {
+                        case ConsoleKey.LeftArrow:
+                            for (int i = 0; i < 4; i++) //rows
+                            {
+                                for (int ii = 0; ii < 3; ii++) //collumns
+                                {
+                                    if (Grid[i, ii] == 0)
+                                    {
+                                        Grid[i, ii] = Grid[i, ii + 1];
+                                        Grid[i, ii + 1] = 0;
+                                    }
+                                    else if (Grid[i, ii] == Grid[i, ii + 1])
+                                    {
+                                        Grid[i, ii] = Grid[i, ii + 1] * 2;
+                                        Grid[i, ii + 1] = 0;
+
+                                        if (Grid[i, ii + 1] * 2 == 2048) throw new Exception("You won the game!");
+                                    }
+                                }
+                            }
+
+                            break;
+
+                        case ConsoleKey.RightArrow:
+                            for (int i = 0; i < 4; i++) //rows
+                            {
+                                for (int ii = 3; ii > 0; ii--) //collumns
+                                {
+                                    if (Grid[i, ii] == 0)
+                                    {
+                                        Grid[i, ii] = Grid[i, ii - 1];
+                                        Grid[i, ii - 1] = 0;
+                                    }
+                                    else if (Grid[i, ii] == Grid[i, ii - 1])
+                                    {
+                                        Grid[i, ii] = Grid[i, ii - 1] * 2;
+                                        Grid[i, ii - 1] = 0;
+
+                                        if (Grid[i, ii - 1] * 2 == 2048) throw new Exception("You won the game!");
+                                    }
+                                }
+                            }
+
+                            break;
+
+                        case ConsoleKey.DownArrow:
+                            for (int i = 0; i < 4; i++) //columns
+                            {
+                                for (int ii = 3; ii > 0; ii--) //rows
+                                {
+                                    if (Grid[ii, i] == 0)
+                                    {
+                                        Grid[ii, i] = Grid[ii - 1, i];
+                                        Grid[ii - 1, i] = 0;
+                                    }
+                                    else if (Grid[ii, i] == Grid[ii - 1, i])
+                                    {
+                                        Grid[ii, i] = Grid[ii - 1, i] * 2;
+                                        Grid[ii - 1, i] = 0;
+
+                                        if (Grid[ii - 1, i] * 2 == 2048) throw new Exception("You won the game!");
+                                    }
+                                }
+                            }
+
+                            break;
+
+                        case ConsoleKey.UpArrow:
+                            for (int i = 0; i < 4; i++) //columns
+                            {
+                                for (int ii = 0; ii < 3; ii++) //rows
+                                {
+                                    if (Grid[ii, i] == 0)
+                                    {
+                                        Grid[ii, i] = Grid[ii + 1, i];
+                                        Grid[ii + 1, i] = 0;
+                                    }
+                                    else if (Grid[ii, i] == Grid[ii + 1, i])
+                                    {
+                                        Grid[ii, i] = Grid[ii + 1, i] * 2;
+                                        Grid[ii + 1, i] = 0;
+
+                                        if (Grid[ii + 1, i] * 2 == 2048) throw new Exception("You won the game!");
+                                    }
+                                }
+                            }
+
+                            break;
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    C.Clear();
+                    C.WriteLine(ex.Message);
+                    break;
+                }
+            }
+
+            C.WriteLine("Press any key to continue.");
+            C.ReadKey();
+        }
+
+        static void AddNumber()
+        {
+            bool isOpen = false;
+            for (int i = 0; i < 4; i++)
+                for (int ii = 0; ii < 4; ii++)
+                    if (Grid[i, ii] == 0) isOpen = true;
+
+            if (isOpen)
+            {
+                Random rnd = new Random();
+                Tuple<int, int> place = new Tuple<int, int>(rnd.Next(0, 4), rnd.Next(0, 4));
+
+                while (Grid[place.Item1, place.Item2] != 0)
+                {
+                    place = new Tuple<int, int>(rnd.Next(0, 4), rnd.Next(0, 4));
+                }
+
+                Grid[place.Item1, place.Item2] = rnd.Next(1, 3) * 2;
+            }
+            else throw new Exception("The game is over.");
+        }
+
+        static void WriteGrid()
+        {
+            C.WriteLine();
+
+            for (int i = 0; i < 4; i++ )
+            {
+                for (int ii = 0; ii < 4; ii++)
+                {
+                    if (Grid[i, ii] != 0)
+                    {
+                        switch (Grid[i, ii].ToString().Length)
+                        {
+                            case 1:
+                                C.Write(" |    {0}", Grid[i, ii].ToString());
+                                break;
+
+                            case 2:
+                                C.Write(" |   {0}", Grid[i, ii].ToString());
+                                break;
+
+                            case 3:
+                                C.Write(" |  {0}", Grid[i, ii].ToString());
+                                break;
+
+                            case 4:
+                                C.Write(" | {0}", Grid[i, ii].ToString());
+                                break;
+                        }
+                    }
+                    else C.Write(" |     ");
+                }
+                C.WriteLine();
+                C.WriteLine("----------------------------");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+Console2048
+===========
+
+Play 2048 in the console! What's not to love?
+
+The mechanics aren't completely there yet. I'm not sure I'll get around to that, this is just a project to pass some boredom.
+
+Pull requests and comments and such are always welcome! Have fun with it.
+
+License
+-------
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Ian Wold
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
